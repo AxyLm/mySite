9d5a9538eafe7b540eb0a42503406d7f916e698d
wy <thelmg@163.com>
2022-07-13 17:56:14 +0800

    chore: eslint prettier husky

--- src/util/abiGenRule.ts
+++ src/util/abiGenRule.ts
@@ -1,13 +1,13 @@
-const defaultClassName = "OneAutoGenFun";
+const defaultClassName = 'OneAutoGenFun';
 const fnGenRule = {
-	read: true,
-	write: false,
-	event: false,
-	nameFilter: undefined,
+  read: true,
+  write: false,
+  event: false,
+  nameFilter: undefined,
 };
 
 export const contractReadRule = (fn: abiFn) => {
-	return `
+  return `
 ${exegesisGen(fn)}
 async ${fn.name}(${parameterGen(fn.inputs)}) {
 	const res = await this.read(
@@ -18,7 +18,7 @@ async ${fn.name}(${parameterGen(fn.inputs)}) {
 }`;
 };
 export const contractWriteRule = (fn: abiFn) => {
-	return `
+  return `
 ${exegesisGen(fn)}
 async ${fn.name}(${parameterGen(fn.inputs)}) {
 	const res = await this.write(
@@ -31,25 +31,25 @@ async ${fn.name}(${parameterGen(fn.inputs)}) {
 `;
 };
 export const parameterGen = (params: abiParam[]) => {
-	return params.map((e) => e.name).join(", ");
+  return params.map((e) => e.name).join(', ');
 };
 export const paramsGen = (params: abiParam[]) => {
-	const str = params.map((e) => {
-		return `\n * @param {${e.type}} ${e.name || e.type}`;
-	});
-	return str;
+  const str = params.map((e) => {
+    return `\n * @param {${e.type}} ${e.name || e.type}`;
+  });
+  return str;
 };
 
 export const exegesisGen = (fn: abiFn) => {
-	return `
+  return `
 /**
- * ${fn.name}${paramsGen(fn.inputs) || ""}
+ * ${fn.name}${paramsGen(fn.inputs) || ''}
  * @returns res
  */`;
 };
 
-export const classMain = (className = defaultClassName, body = "") => {
-	return `
+export const classMain = (className = defaultClassName, body = '') => {
+  return `
 class ${className} extends IERC20 {
 ${body}
 }
@@ -64,7 +64,7 @@ export function create${className}(adr) {
 };
 
 export const ImportCode = (className = defaultClassName) => {
-	return `
+  return `
 import ABI_${className} from "./abis/${className}.json"
 import IERC20 from "../common/IERC20.js"
 import * as web3util from "../common/web3util.js"
@@ -73,51 +73,55 @@ import { ethers } from 'ethers';
 	`;
 };
 
-export const autoGenClass = (className = defaultClassName, abiJson: abiFn[] = [], filter = fnGenRule) => {
-	const ClassBody = abiJson
-		.map((e) => {
-			if (e.type === "function") {
-				if (filter.nameFilter) {
-					const nameFilter: string = filter.nameFilter;
-					let nameFilters: string[] = [nameFilter];
-					if (nameFilter.indexOf(",") > -1) {
-						nameFilters = nameFilter.split(",");
-					}
+export const autoGenClass = (
+  className = defaultClassName,
+  abiJson: abiFn[] = [],
+  filter = fnGenRule,
+) => {
+  const ClassBody = abiJson
+    .map((e) => {
+      if (e.type === 'function') {
+        if (filter.nameFilter) {
+          const nameFilter: string = filter.nameFilter;
+          let nameFilters: string[] = [nameFilter];
+          if (nameFilter.indexOf(',') > -1) {
+            nameFilters = nameFilter.split(',');
+          }
 
-					if (nameFilters.every((filter) => e.name.indexOf(filter) == -1)) {
-						return undefined;
-					}
-				}
+          if (nameFilters.every((filter) => e.name.indexOf(filter) == -1)) {
+            return undefined;
+          }
+        }
 
-				if (filter.read && e.stateMutability === "view") {
-					const str = contractReadRule(e);
-					return str;
-				}
+        if (filter.read && e.stateMutability === 'view') {
+          const str = contractReadRule(e);
+          return str;
+        }
 
-				if (filter.write && e.stateMutability === "nonpayable") {
-					const str = contractWriteRule(e);
-					return str;
-				}
-			}
+        if (filter.write && e.stateMutability === 'nonpayable') {
+          const str = contractWriteRule(e);
+          return str;
+        }
+      }
 
-			return undefined;
-		})
-		.filter((e) => !!e)
-		.join("");
-	return `
+      return undefined;
+    })
+    .filter((e) => !!e)
+    .join('');
+  return `
 ${ImportCode(className)}
 ${classMain(className, ClassBody)}`;
 };
 
 export type abiFn = {
-	inputs: abiParam[];
-	name: string;
-	outputs: abiParam[];
-	stateMutability: "view" | "nonpayable" | "event";
-	type: "function" | "constructor";
+  inputs: abiParam[];
+  name: string;
+  outputs: abiParam[];
+  stateMutability: 'view' | 'nonpayable' | 'event';
+  type: 'function' | 'constructor';
 };
 export type abiParam = {
-	internalType: string;
-	name: string;
-	type: string;
+  internalType: string;
+  name: string;
+  type: string;
 };
